c8600b8f964b6fd6c529c13b29095d3be50d01a1
Florencia D'Andrea <florencia.dandrea@gmail.com>
2023-03-08 08:29:12 -0800

    Add leaflet-dash.py

--- leaflet-dash.py
+++ leaflet-dash.py
@@ -1,27 +1,11 @@
-from dash import dash, dcc, html, Input, Output
-import plotly.express as px
+# https://github.com/thedirtyfew/dash-leaflet
+import dash
+import dash_leaflet as dl
 
-colorscales = px.colors.named_colorscales()
-app = dash.Dash(__name__)
-app.layout = html.Div([
-    html.H4('Interactive color scale'),
-    html.P("Select your palette:"),
-    dcc.Dropdown(
-        id='dropdown', 
-        options=colorscales,
-        value='viridis'
-    ),
-    dcc.Graph(id="graph"),
-])
+app = dash.Dash()
+app.layout = dl.Map(dl.TileLayer(),
+                     style={'width': '1000px', 
+                            'height': '500px'})
 
-@app.callback(
-    Output("graph", "figure"), 
-    Input("dropdown", "value"))
-def change_colorscale(scale):
-    df = px.data.iris() # replace with your own data source
-    fig = px.scatter(
-        df, x="sepal_width", y="sepal_length", 
-        color="sepal_length", color_continuous_scale=scale)
-    return fig
-    
-app.run_server(debug=True) 
+if __name__ == '__main__':
+    app.run_server()
